e0c81d2c41b99ecd7e3e39f8ec23ccee33f1c9c1
MWise707 <markezrawiseman@gmail.com>
2024-02-19 12:09:53 -0800

    change star image location and import

--- client/components/Accordion/CustomerReviewsContent/CReviewHeaderColumn1.jsx
+++ client/components/Accordion/CustomerReviewsContent/CReviewHeaderColumn1.jsx
@@ -1,38 +1,65 @@
 import { useAppContext } from "./../../AppContext";
-
+import starIcon from "client/assets/Star.png";
 
 function CReviewHeaderColumn1() {
-    const {
-      averageRating,
-      totalReviews,
-      recommendationPercentage,
-    } = useAppContext();
+  const { averageRating, totalReviews, recommendationPercentage } =
+    useAppContext();
 
-    return (
-
-        <div className="column1-header">
-            <li>
-                <div className="list-review-overview">
-                    <span className="ratings_avg">{averageRating}</span>
-                    <span className="reviewsOverviewHeader">
-                        <span className="starsFromReviews">
-                            <span><img src="./components/Accordion/CustomerReviewsContent/imgStar/Star.png" alt="Star" style={{ width: '15px', height: '15px' }} /></span>
-                            <span><img src="./components/Accordion/CustomerReviewsContent/imgStar/Star.png" alt="Star" style={{ width: '15px', height: '15px' }} /></span>
-                            <span><img src="./components/Accordion/CustomerReviewsContent/imgStar/Star.png" alt="Star" style={{ width: '15px', height: '15px' }} /></span>
-                            <span><img src="./components/Accordion/CustomerReviewsContent/imgStar/Star.png" alt="Star" style={{ width: '15px', height: '15px' }} /></span>
-                            <span><img src="./components/Accordion/CustomerReviewsContent/imgStar/Star.png" alt="Star" style={{ width: '15px', height: '15px' }} /></span>
-                        </span>
-                        <span className="outOfTotalReviews">out of {totalReviews} reviews</span>
-                    </span>
-                </div>
-            </li>
-            <li className="recommendWrapper">
-                <span className="percentageOfRec">{recommendationPercentage}%</span>
-                <span className="recommendProduct"> recommend this product</span>
-            </li>
-
+  return (
+    <div className="column1-header">
+      <li>
+        <div className="list-review-overview">
+          <span className="ratings_avg">{averageRating}</span>
+          <span className="reviewsOverviewHeader">
+            <span className="starsFromReviews">
+              <span>
+                <img
+                  src={starIcon}
+                  alt="Star"
+                  style={{ width: "15px", height: "15px" }}
+                />
+              </span>
+              <span>
+                <img
+                  src={starIcon}
+                  alt="Star"
+                  style={{ width: "15px", height: "15px" }}
+                />
+              </span>
+              <span>
+                <img
+                  src={starIcon}
+                  alt="Star"
+                  style={{ width: "15px", height: "15px" }}
+                />
+              </span>
+              <span>
+                <img
+                  src={starIcon}
+                  alt="Star"
+                  style={{ width: "15px", height: "15px" }}
+                />
+              </span>
+              <span>
+                <img
+                  src={starIcon}
+                  alt="Star"
+                  style={{ width: "15px", height: "15px" }}
+                />
+              </span>
+            </span>
+            <span className="outOfTotalReviews">
+              out of {totalReviews} reviews
+            </span>
+          </span>
         </div>
-    )
+      </li>
+      <li className="recommendWrapper">
+        <span className="percentageOfRec">{recommendationPercentage}%</span>
+        <span className="recommendProduct"> recommend this product</span>
+      </li>
+    </div>
+  );
 }
 
 export default CReviewHeaderColumn1;
