ca614193403ccc5313682ad80364da5de61388b4
MWise707 <markezrawiseman@gmail.com>
2024-02-19 10:41:08 -0800

    hd logo fix

--- client/components/Header/Header.jsx
+++ client/components/Header/Header.jsx
@@ -1,6 +1,7 @@
-import "./header.css"
+import "./header.css";
+import hdLogo from "client/assets/hdLogo.png";
 
-const Header = ({itemsInCart}) => {
+const Header = ({ itemsInCart }) => {
   return (
     <>
       <header>
@@ -10,11 +11,15 @@ const Header = ({itemsInCart}) => {
         <div id="header-root">
           <div className="header-content">
             <div className="side-by-side" id="home-depot-logo">
-              <img src="./components/Header/header-images/HDLogo.png" alt="Home Depot Logo" />
+              <img src={hdLogo} alt="Home Depot Logo" />
             </div>
             <div className="side-by-side" id="store-loc-time-zip">
               <div className="location-divs" id="storeLogo">
-                <img id="storePic" src="./components/Header/header-images/store.png" alt="Store Icon" />
+                <img
+                  id="storePic"
+                  src="./components/Header/header-images/store.png"
+                  alt="Store Icon"
+                />
               </div>
               <div className="location-divs" id="storeLocation">
                 SW Color...
@@ -23,7 +28,11 @@ const Header = ({itemsInCart}) => {
                 9PM
               </div>
               <div className="location-divs" id="truck">
-                <img id="truckPic" src="./components/Header/header-images/truck.png" alt="Truck Icon" />
+                <img
+                  id="truckPic"
+                  src="./components/Header/header-images/truck.png"
+                  alt="Truck Icon"
+                />
               </div>
               <div className="location-divs" id="storeZip">
                 86753
@@ -37,18 +46,38 @@ const Header = ({itemsInCart}) => {
                 placeholder="What can we help you find today?"
               />
               <button onClick={() => performSearch()}>
-                <img src="./components/Header/header-images/search.png" alt="Search Icon" />
+                <img
+                  src="./components/Header/header-images/search.png"
+                  alt="Search Icon"
+                />
               </button>
             </div>
             <div className="side-by-side" id="navIcons">
-              <img src="./components/Header/header-images/bucket.png" alt="Bucket Icon" />
-              <img src="./components/Header/header-images/services.png" alt="Services Icon" />
-              <img src="./components/Header/header-images/DIY.png" alt="DIY Icon" />
-              <img src="./components/Header/header-images/me.png" alt="Me Icon" />
+              <img
+                src="./components/Header/header-images/bucket.png"
+                alt="Bucket Icon"
+              />
+              <img
+                src="./components/Header/header-images/services.png"
+                alt="Services Icon"
+              />
+              <img
+                src="./components/Header/header-images/DIY.png"
+                alt="DIY Icon"
+              />
+              <img
+                src="./components/Header/header-images/me.png"
+                alt="Me Icon"
+              />
             </div>
             <div className="side-by-side" id="cartIcon">
-              <img src="./components/Header/header-images/cart.png" alt="Cart Logo" />
-              <div style={{display: (itemsInCart == 0) ? 'none' : 'flex'}}><span>{itemsInCart}</span></div>
+              <img
+                src="./components/Header/header-images/cart.png"
+                alt="Cart Logo"
+              />
+              <div style={{ display: itemsInCart == 0 ? "none" : "flex" }}>
+                <span>{itemsInCart}</span>
+              </div>
             </div>
           </div>
         </div>
